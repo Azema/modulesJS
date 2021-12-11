f278fce00a69ee05b9090eb0c1af12cc8a78ee1c
Azema <manuel.hervo@gmail.com>
2021-12-11 16:48:28 +0100

    Modif module index

--- index.mjs
+++ index.mjs
@@ -1,9 +1,9 @@
 'use strict';
 import { Cache, Media, Show, Movie, Episode, Similar } from './Medias.mjs';
 
-var window.Cache = Cache;
-var window.Media = Media;
-var window.Show = Show;
-var window.Movie = Movie;
-var window.Episode = Episode;
-var window.Similar = Similar;
+window.Cache = Cache;
+window.Media = Media;
+window.Show = Show;
+window.Movie = Movie;
+window.Episode = Episode;
+window.Similar = Similar;
